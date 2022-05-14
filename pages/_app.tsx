4fb321726c3d7f4a4abb210a24dc9cd8cc7b521b
NomanTkxel <noman.shahid@tkxel.com>
2022-05-14 22:09:22 +0500

    Final boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import type { AppProps } from "next/app";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/api/graphql",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
